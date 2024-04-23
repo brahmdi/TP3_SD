b717a9b3f0b7501a00174476cac98f727be5737b
brahmdi <96790699+brahmdi@users.noreply.github.com>
2024-04-23 01:41:23 +0100

    Add files via upload

---
+++ hospitalApp2/src/main/java/com/tp/hospitalapp2/HospitalApp2Application.java
@@ -0,0 +1,13 @@
+package com.tp.hospitalapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalApp2Application.class, args);
+	}
+
+}
---
+++ hospitalApp2/src/test/java/com/tp/hospitalapp2/HospitalApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.hospitalapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
